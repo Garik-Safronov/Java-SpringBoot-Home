ab3869a9f1bd6a3cf2dd8b419f9c8ec131c11e40
Garik-S <garik.safronov@gmail.com>
2022-04-11 15:21:40 +0300

    Initial commit

---
+++ src/main/java/ru/gb/javaspringboothome/JavaSpringBootHomeApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.javaspringboothome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootHomeApplication.class, args);
+    }
+
+}
